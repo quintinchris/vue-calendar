06045ad45b0c295bb2e00d6fa1c8ad4cbcf4536c
QuintinChris <cwq23drexel@gmail.com>
2020-06-20 10:43:57 -0400

    added calendarEvent

--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,16 @@
-/* eslint-disable import/prefer-default-export */
+/* eslint-disable */
 import { seedData } from './seed';
 
 export const store = {
   state: {
     seedData,
   },
+  getActiveDay() {
+    return this.state.seedData.find((day) => day.active);
+  },
+  setActiveDay() {
+    this.state.seedData.map((dayObj) => {
+      dayObj.id === dayId ? dayObj.active = true : dayObj.active = false;
+    });
+  }
 };
